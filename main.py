9fd48b30a7a8abe077ae3960da60b34d8f343620
Doris2000 <127191129+Doris2000@users.noreply.github.com>
2023-03-28 10:48:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,6 @@
-numbers=[3,7,9,12]
-sum=0
-first,second,thrid,fourth=numbers
-sum=first+second+thrid+fourth
-
-print(sum)
-
-
-def smallest_numin_list (list):
-    min=list[0]
-    for i in list:
-        if i<min:
-            min=i
-    return min
-
-print(smallest_numin_list([1,9,8888]))
-
-print(sorted(numbers,reverse=True))
-
-items=[("b",1),
-       ("c",7),
-       ("b",134)]
-
-
-items.sort(key=lambda item:item[1])
-print(items)
-
-
-students=[
-    {"imie":"Jan","wiek":29},
-    {"imie":"Janik","wiek":28},
-    {"imie":"Jola","wiek":27}
-]
-students.sort(key=lambda student:student["wiek"])
-print(students[0])
-
-#mapa-zmiana elementu
-
-items1=[
-    ("Produkt1",10)
-
-]
+numbers=[1,2,3,4]
+suma=0
+for number in range(len(numbers)):
+    suma+=numbers[number]
+    numbers[number]=suma
+print(numbers)
