129c18eef5627e9d57e02a7c43c30f6d6aa32107
Doris2000 <127191129+Doris2000@users.noreply.github.com>
2023-04-04 10:50:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-numbers=[1,2,3,4]
-suma=0
-for number in range(len(numbers)):
-    suma+=numbers[number]
-    numbers[number]=suma
-print(numbers)
+# - function to find youngest student from a set
+students = [{"imie": "Tomek", "wiek": 20}, {"imie": "Karolina", "wiek": 18}]
+
+def sort_item(item):
+    return item["wiek"]
+
+students.sort(key=sort_item)
+print(students)
+najmlodszy=list(filter(lambda item: item["wiek"]==18, students))
+print(najmlodszy)
+
+#zad 2
+def ile_razy_zmiana(wartosc):
+    ile=0
+    for i in range(len(wartosc)-1):
+        if wartosc[i]!=wartosc[i+1]:
+            ile+=1
+    return ile
+with open("mecz.txt", "r") as file:
+    results=file.readline()
+
+print(ile_razy_zmiana(results))
